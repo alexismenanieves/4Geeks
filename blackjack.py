97ec57d308b3918a13c0f331d9bc4b64a5917254
Alexis Mena <alexismenanieves@hotmail.com>
2024-11-08 22:50:00 -0500

    Lint blackjack file

--- blackjack.py
+++ blackjack.py
@@ -1,26 +1,33 @@
 import random
 
+
 class Card:
     def __init__(self, suit, value, card_value):
         self.suit = suit
         self.value = value
         self.card_value = card_value
-    
+
     def __repr__(self):
         return f'({self.suit},{self.value})'
 
-suits = ['Spades','Hearts','Clubs','Diamonds']
-suits_values = {'Spades': '\u2664', 'Hearts':'\u2661', 
-               'Clubs': '\u2667', 'Diamonds':'\u2662'}
-cards = ['A','2','3','4','5','6','7','8','9','10','J','Q','K']
-cards_values = {'A':11, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7, 
-                '8':8, '9':9, '10':10, 'J':10, 'Q':10, 'K':10}
+
+suits = ['Spades', 'Hearts', 'Clubs', 'Diamonds']
+suits_values = {
+    'Spades': '\u2664', 'Hearts':  '\u2661',
+    'Clubs': '\u2667', 'Diamonds': '\u2662'
+}
+cards = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+cards_values = {
+    'A': 11, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7,
+    '8': 8, '9': 9, '10': 10, 'J': 10, 'Q': 10, 'K': 10
+}
 
 # Let's generetate a deck
 deck = []
 for suit in suits:
     for card in cards:
         deck.append(Card(suits_values[suit], card, cards_values[card]))
+
 
 def blackjack_game(deck):
     global cards_values
@@ -38,7 +45,7 @@ def blackjack_game(deck):
             if player_cards[0].card_value == 11 and player_cards[1].card_value == 11:
                 player_cards[0].card_value = 1
                 player_score -= 10
-        
+     
         print('Player cards: ')
         print(repr(player_cards))
         print(f'Player score: {player_score}')
@@ -48,6 +55,7 @@ def blackjack_game(deck):
             print('You have a Blackjack!')
             print('You win!')
             quit()
+
 
 def blackjack_game_rep():
     global cards_values
@@ -78,10 +86,11 @@ def blackjack_game_rep():
 
     return result
 
+
 acum = 0
 n = 10000
 
-for i in range(n): 
+for i in range(n):
     acum += blackjack_game_rep()
 
 print(f'The average of Natural 21 is: {acum/n}')
